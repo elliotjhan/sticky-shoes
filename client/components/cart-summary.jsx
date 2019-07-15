c5089d5e3dd900167cf568803a7da22d55bfcece
Elliot Han <elliotjhan@gmail.com>
2019-07-14 20:17:38 -0700

    user can checkout

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -22,6 +22,13 @@ class CartSummary extends React.Component {
     callback(catalog, params);
   }
 
+  setViewCheckout() {
+    let callback = this.props.setView;
+    let checkout = 'checkout';
+    let params = {};
+    callback(checkout, params);
+  }
+
   render() {
     var cartItemArray = this.props.cart;
     var cartItemArrayDisplay = null;
@@ -41,8 +48,11 @@ class CartSummary extends React.Component {
           <div>
             {cartItemArrayDisplay}
           </div>
-          <div className="row">
-            <h2 className="col text-right mt-3">Item Total ${this.getCartTotal()}</h2>
+          <div className="row align-items-center">
+            <h2 className="col text-left mt-3">Cart Total ${this.getCartTotal()}</h2>
+            <div className="col text-right ">
+              <button onClick={this.setViewCheckout.bind(this)} className="btn btn-primary">Checkout</button>
+            </div>
           </div>
         </div>
       );
